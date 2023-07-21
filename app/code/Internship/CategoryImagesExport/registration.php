f132a33377eb51965e127a51062d66c6a40fed66
tomk1v <tomkivandrii18@gmail.com>
2023-07-21 12:36:05 +0300

    adding form information for export, code refactoring

--- app/code/Internship/CategoryImagesExport/registration.php
+++ app/code/Internship/CategoryImagesExport/registration.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Category Images Export
+ *
+ * @category  Internship
+ * @package   Internship\CategoryImagesExport
+ * @author    Andrii Tomkiv <tomkivandrii18@gmail.com>
+ * @copyright 2023 tomk1v
+ */
+
 use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(
