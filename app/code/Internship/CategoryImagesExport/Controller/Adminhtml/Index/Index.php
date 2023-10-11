5470c94d33ffef0b35f119afc38d0b08349eb051
tomk1v <tomkivandrii18@gmail.com>
2023-10-11 17:34:48 +0300

    realizing logic of catalog images export

---
+++ app/code/Internship/CategoryImagesExport/Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Category Images Export
+ *
+ * @category  Internship
+ * @package   Internship\CategoryImagesExport
+ * @author    Andrii Tomkiv <tomkivandrii18@gmail.com>
+ * @copyright 2023 tomk1v
+ */
+
+namespace Internship\CategoryImagesExport\Controller\Adminhtml\Index;
+
+class Index extends \Magento\Backend\App\Action
+{
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param \Magento\Backend\App\Action\Context                $context
+     * @param \Magento\Framework\View\Result\PageFactory         $resultPageFactory
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Product Menu admin page
+     *
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('Internship_CategoryImagesExport::home');
+        $resultPage->getConfig()->getTitle()->prepend((__('Category Images Export')));
+
+        return $resultPage;
+    }
+}
